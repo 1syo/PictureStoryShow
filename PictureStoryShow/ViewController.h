b9fc0310c7ae44f61c1b22212d8fd427c952fe6c
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-02-05 23:11:19 +0900

    Initial Commit

---
+++ PictureStoryShow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PictureStoryShow
+//
+//  Created by TAKAHASHI Kazunari on 2014/02/05.
+//  Copyright (c) 2014年 TAKAHASHI Kazunari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PictureStoryShow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PictureStoryShow
+//
+//  Created by TAKAHASHI Kazunari on 2014/02/05.
+//  Copyright (c) 2014年 TAKAHASHI Kazunari. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
