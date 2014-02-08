59b92b0afee1d725f7f1cdf223d8517dc1852821
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-02-08 21:21:48 +0900

    画面をつくった

--- PictureStoryShow/ViewController.h
+++ PictureStoryShow/ViewController.h
@@ -9,5 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+- (IBAction)nextButton:(id)sender;
+- (IBAction)prevButton:(id)sender;
 
 @end
